c64a5837003ee90f5bf0adfce84b7882ea5f131c
poke0923 <orbis.9482@gmail.com>
2023-11-04 14:31:56 +0900

    投稿フォーム内容の登録とエラーメッセージ表示

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -10,19 +10,21 @@
             Blog Name
         </h1>
         
-        <form action="/posts" method="post" >
+        <form action="/posts" method="POST" >
         @csrf
         <div class = "title">
             <h2>タイトル</h2>
-            <p><input type="text" name="post[title]"　placeholder="タイトル"/></p>
+            <input type="text" name="post[title]"　placeholder="タイトル" value="{{ old('post.title') }}"/>
+            <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
         </div>
         
         <div class ="body">
             <h3>本文</h3>
-            <textarea name="post[body]" cols="60" rows="20" placeholder="本文"></textarea>
+            <textarea name="post[body]" cols="60" rows="20" placeholder="本文">{{ old('post.body') }}</textarea>
+            <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
         </div>    
 
-        <input type="submit" value="保存">
+        <input type="submit" value="store">
         </form>
         
         <div class="footer">
